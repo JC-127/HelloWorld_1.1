594c586d4e67a0a0552e5ea4e93a0134f49aa97c
Juan-Carlo V. Mercado <tuk35310@temple.edu>
2021-09-14 12:19:10 -0400

    SDK was changed to the proper SDK

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -7,8 +7,8 @@ import android.widget.Button
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        findViewById<Button>(R.id.button)
-            .setOnClickListener({v, -> (v as Button).text = "You clicked the button"})
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.button)
+            .setOnClickListener { v, -> (v as Button).text = "You clicked the button" }
     }
 }
